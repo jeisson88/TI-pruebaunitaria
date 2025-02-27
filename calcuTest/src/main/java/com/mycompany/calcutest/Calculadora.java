6a102a66320421cadef82e43830b972ec8c4d3af
jeisson88 <jlopezh36@miumg.edu.gt>
2025-02-27 06:26:06 +0100

    Add files via upload
    proyecto fase final

--- calcuTest/src/main/java/com/mycompany/calcutest/Calculadora.java
+++ calcuTest/src/main/java/com/mycompany/calcutest/Calculadora.java
@@ -19,4 +19,33 @@ public class Calculadora {
     public int sumar(int a, int b) {
         return a + b; // Retorna el resultado de la suma.
     }
+    
+    // metodo para resta
+        public int restar(int a, int b) {
+        return a - b;
+    }
+        
+     /**
+     * Método para multiplicar dos números enteros.
+     * @param a Primer número
+     * @param b Segundo número
+     * @return El producto de a y b
+     */
+    public int multiplicar(int a, int b) {
+        return a * b;
+    }
+
+    /**
+     * Método para dividir dos números enteros.
+     * @param a Dividendo
+     * @param b Divisor (Debe ser diferente de 0)
+     * @return El resultado de la división
+     * @throws ArithmeticException si b es 0
+     */
+    public double dividir(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("No se puede dividir por cero");
+        }
+        return (double) a / b;
+    }
 }
